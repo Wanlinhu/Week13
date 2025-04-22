d4272ce152504bf82beca4ef3da61af3f495fc9d
Wanlin Huang <wanlinhuang@Wanlins-MacBook-Pro.local>
2025-04-22 18:56:35 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,12 @@
+playlist = [
+    "The First Impression — Shigeru Nagata",
+    "I Wish You Love — Laufey",
+    "《珠玉》— 单依纯",
+    "Like Jennie — Jennie",
+    "No Sass — Photay"
+]
+
+playlist.append("I'm fallin' out of love with you, I don't know what to do... — Fallin' Out by Keyshia Cole")
+
+print(playlist[4])
+mix_of_stuff = [42, "dog", 3.14, True]
